611d651a8f4296f744e63b37770066fd965a579f
oxg558 <oxg558@cs.bham.ac.uk>
2017-08-17 14:33:07 +0100

    Mergesort Haskell

--- src/sorting/listsorts.hs
+++ src/sorting/listsorts.hs
@@ -19,7 +19,27 @@ ssort [] = []
 ssort xs = let n = minimum xs; l = delete n xs in n : ssort l
 
 -- MERGE SORT --
--- TODO
+   -- v1: top-down
+halve (x:xs) = let (h1,h2) = halve xs in ((x:h2),h1)
+halve xs      = (xs,[])
+
+merge list [] = list
+merge [] list = list
+merge l1@(x:xs) l2@(y:ys) | x <= y     = x : (merge xs l2)
+                          | otherwise = y : (merge ys l1) 
+
+msort []     = []
+msort [x]    = [x]
+msort xs     = merge (msort a) (msort b) where (a,b) = halve xs
+
+   -- v2: bottom-up (from RosettaCode.org)
+mergePairs (sorted1 : sorted2 : sorteds) = merge sorted1 sorted2 : mergePairs sorteds
+mergePairs sorteds = sorteds
+  
+mergeAll [sorted] = sorted
+mergeAll sorteds = mergeAll (mergePairs sorteds)
+
+msort2 list = mergeAll (map (\x -> [x]) list)
 
 -- QUICK SORT --
    -- v1: generators
